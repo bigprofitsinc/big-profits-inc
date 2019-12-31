2afc1d1b26c19d1cc197177053f0b06b8499971a
bigprofitsinc <58669479+bigprofitsinc@users.noreply.github.com>
2019-12-31 23:16:03 +0530

    Add files via upload

---
+++ webpage/js/main.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+	
+	$(document).ready(function() {
+		
+		jQuery('.counter-item').appear(function() {
+			jQuery('.counter-number').countTo();
+			jQuery(this).addClass('funcionando');
+			console.log('funcionando');
+		});
+		
+	}); 
+	
+	// Some user deleted the header page and the below code resulted in Syntax error
+	try {
+		var distance = $('.header_lower_row').offset().top;
+		
+		$(window).scroll(function() {
+			
+			if($(this).scrollTop() >= distance){
+				$('.header_lower_row').addClass('header_shrink'); 
+			} 
+			 else{
+				$('.header_lower_row').removeClass('header_shrink'); 
+			} 
+		 
+		});
+	}
+	catch(err) {
+		//
+	}
+	
+});
